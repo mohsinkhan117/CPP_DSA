0babc4df92948c25a40db037a5f4fccdc6ed2f4c
mohsin117 <mohsinkhanmandan@gmail.com>
2026-03-09 14:04:40 +0500

    Done with sorting algorithm

--- DSA/sem_4_DSA_lab/lab_04/lec.cpp
+++ DSA/sem_4_DSA_lab/lab_04/lec.cpp
@@ -1,4 +1,5 @@
 // buble sort
+// average sort
 //  Example array:  [5,3,8,4,2]
 // traditional sorting algorithm using temporary variabe for storing the old variables
 #include <iostream>
@@ -24,6 +25,7 @@ void bubble_sort(int arr[], int size)
     }
 }
 // selection sort
+// worst algorithm
 void selection_sort(int arr[], int size)
 {
     int min;
@@ -37,6 +39,9 @@ void selection_sort(int arr[], int size)
                 min = j;
             }
             swap(arr[i], arr[min]);
+            // int temp = arr[j];
+            // arr[j] = arr[j + 1];
+            // arr[j + 1] = temp;
         }
     }
     cout << "\nSelection sort: ";
@@ -44,11 +49,28 @@ void selection_sort(int arr[], int size)
     {
         cout << arr[i] << " ";
     }
-
-    // bubble_sort(arr, size);
-    // int temp = arr[j];
-    // arr[j] = arr[j + 1];
-    // arr[j + 1] = temp;
+}
+// insertion sort
+// best sorting algorithm
+void insertion_sort(int arr[], int size)
+{
+    int key, j;
+    for (int i = 1; i < size; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+    cout << "\nInsertion sort: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
 }
 int main()
 {
@@ -60,4 +82,5 @@ int main()
     }
     bubble_sort(arr, 5);
     selection_sort(arr, 5);
+    insertion_sort(arr, 5);
 }
