2cbc7554630a60c9f73af270c606c854c9453781
mohsinkhan117 <mohsinkhanmandan@gmail.com>
2026-06-04 11:29:28 -0700

    Done Binary Search Algorithm

--- DSA_lab/sem_4_DSA_lab/lab_02/task_02_binary_search.cpp
+++ DSA_lab/sem_4_DSA_lab/lab_02/task_02_binary_search.cpp
@@ -1,22 +1,10 @@
 #include <iostream>
 using namespace std;
-
-int linearSearch(int arr[], int size, int value)
-{
-    for (int i = 0; i < size; i++)
-    {
-        if (arr[i] == value)
-        {
-            return i;
-        }
-    }
-    return -1;
-}
-
 int binarySearch(int arr[], int size, int value)
 {
     int low = 0;
     int high = size - 1;
+
     while (low <= high)
     {
         int mid = low + (high - low) / 2;
@@ -35,25 +23,11 @@ int binarySearch(int arr[], int size, int value)
     }
     return -1;
 }
-
 int main()
 {
-
     int arr[] = {2, 6, 7, 8, 10};
     int target = 10;
     int size = sizeof(arr) / sizeof(arr[0]);
-
-    cout << "Linear Search: \n"
-         << endl;
-    int linearResult = linearSearch(arr, size, target);
-    if (linearResult != -1)
-    {
-        cout << target << " Found at index " << linearResult << endl;
-    }
-    else
-    {
-        cout << target << " not found." << endl;
-    }
 
     cout << "\nBinary Search: \n"
          << endl;
